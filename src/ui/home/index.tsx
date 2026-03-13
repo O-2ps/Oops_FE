72453c1e356269f102fe3a094ba824c7af9fe10e
dpqlsn <a01037111209@gmail.com>
2026-03-13 10:43:25 +0900

    feat :: p1 퍼블리싱

---
+++ src/ui/home/index.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import * as S from "./style";
+
+export default function Home() {
+    return (
+        <S.Container>
+            <S.GreenBox />
+            <S.MainContent>
+                <S.Header>
+                    <S.StepNumber>1.</S.StepNumber>
+                    <S.Title>퍼스널 컬러 분석하기</S.Title>
+                </S.Header>
+
+                <S.WheelSection>
+                    <S.ArrowButton>&lt;</S.ArrowButton>
+                    <S.ColorWheel src="/assets/colors.svg" alt="Color Wheel" />
+                    <S.ArrowButton>&gt;</S.ArrowButton>
+                </S.WheelSection>
+                <S.FooterAction>
+                    [ 분석 시작하기 ]
+                </S.FooterAction>
+            </S.MainContent>
+            <S.GreenBox />
+        </S.Container>
+    );
+}
