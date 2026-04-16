a32c01c9ceafc20985f913b10ef8209966ffb912
dpqlsn <a01037111209@gmail.com>
2026-04-16 12:20:57 +0900

    chore :: 라우터 설정

--- src/ui/mypage/index.tsx
+++ src/ui/mypage/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     };
 
     const handleLoginRedirect = () => {
-        console.log("Redirecting to login...");
+        router.push("/");
     };
 
     return (
